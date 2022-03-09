e740dd27941b5d1db027568c476707caf485a8ee
Ivana-R98 <ivannarosic98@gmail.com>
2022-03-09 19:06:56 +0100

    messy code af, left with rainbow button todo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sketch
 Etch-a-sketch project from The Odin Project
+
+
+# create a div container that will contain the grid (16x16) of divs, take a clear part of the page for that
+
+## make the grid in JavaScript (and responsive (up to 100) later)
+
+### Set up a “hover” effect so that the grid divs change color when your mouse passes over them
+
+#### Add a button which clears the current grid and sends user a popup asking for the number of squares for the new grid
+
+# OPTIONAL: Instead of just changing the color of a square from one color to another, have each pass through with the mouse change it to a completely random RGB value
---
+++ app.js
@@ -0,0 +1,95 @@
+let row = 16;
+let col = 16;
+let grid = row*col;
+let color = "black";
+
+let container = document.querySelector(".container");
+
+//change colors to selected from dropdown
+let colorSelected = document.querySelectorAll(".color");
+colorSelected.forEach(colors => {
+    colors.addEventListener("click", (e) => {
+        color = e.target.innerHTML;
+    });
+});
+
+//selecting the grid
+let gridSelect = document.getElementById("gridSelect").
+    addEventListener("click", () => {
+        let gridSize;
+        //prompt the user for the grid size
+        do {
+            gridSize = prompt("What size would you like the grid to be?", "16");
+        }
+        while (gridSize > 100);
+        row = +gridSize;
+        col = +gridSize;
+        grid = row*col;
+        clearCurrentGrid();
+        create();
+})
+
+// clear the current grid
+function clearCurrentGrid () {
+    container.innerHTML = ''
+}
+
+// make a loop for creating the grid
+function create () {
+    container.style.gridTemplateColumns = `repeat(${row}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${col}, 1fr)`;
+    let mouseDown = false;    
+    for(let i = 0; i < grid; i++) {
+        let div = document.createElement("div"); 
+
+        // div.innerHTML = "a";
+        container.appendChild(div);
+
+        // add event listeners to change colors  
+        div.addEventListener("mousedown", () => {
+            div.style.backgroundColor = color;
+            mouseDown = true;
+        })
+        div.addEventListener("mouseup", () => {
+            mouseDown = false;
+        })
+        div.addEventListener("mousemove", () => {
+            if(mouseDown) {
+                div.style.backgroundColor = color;
+            }
+        })
+    }
+}
+
+//delete the color, eraser
+let eraser = document.getElementById("eraser").
+    addEventListener("click", () => {
+       color = "white"; 
+})
+
+
+//dropdown button effects
+function drop() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+  
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
+let reload = document.querySelector("#reset").
+    addEventListener("click", () => {
+        location.reload();
+})
+
+create();
