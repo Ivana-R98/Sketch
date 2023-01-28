9ddddf08502aec7043dece93475dc9d2d3af4f8c
Ivana-R98 <ivannarosic98@gmail.com>
2023-01-28 11:37:54 +0100

    made rainbow button, made site responsive, changed color button

--- app.js
+++ app.js
@@ -2,36 +2,37 @@ let row = 16;
 let col = 16;
 let grid = row*col;
 let color = "black";
+let mode = 'color';
 
 let container = document.querySelector(".container");
 
-//change colors to selected from dropdown
-let colorSelected = document.querySelectorAll(".color");
-colorSelected.forEach(colors => {
-    colors.addEventListener("click", (e) => {
-        color = e.target.innerHTML;
-    });
-});
-
-//selecting the grid
-let gridSelect = document.getElementById("gridSelect").
-    addEventListener("click", () => {
-        let gridSize;
-        //prompt the user for the grid size
-        do {
-            gridSize = prompt("What size would you like the grid to be?", "16");
-        }
-        while (gridSize > 100);
-        row = +gridSize;
-        col = +gridSize;
-        grid = row*col;
-        clearCurrentGrid();
-        create();
+//color picker
+let colorpicker = document.getElementById("colorpicker");
+colorpicker.addEventListener("input", () => {
+    let picked = colorpicker.value;
+        mode = 'color';
+        color = picked;
 })
+
+let display = document.getElementById("gridDisplay");
+let rangeSelect = 16;
+let range = document.getElementById("gridrange");
+range.addEventListener('input', () => {
+    rangeSelect = range.value;
+    display.innerText = rangeSelect;
+    gridSize = rangeSelect;
+    row = +gridSize;
+    col = +gridSize;
+    grid = row*col;
+    clearCurrentGrid();
+    create();
+}) 
+
+
 
 // clear the current grid
 function clearCurrentGrid () {
-    container.innerHTML = ''
+    container.innerHTML = '';
 }
 
 // make a loop for creating the grid
@@ -41,21 +42,19 @@ function create () {
     let mouseDown = false;    
     for(let i = 0; i < grid; i++) {
         let div = document.createElement("div"); 
-
-        // div.innerHTML = "a";
         container.appendChild(div);
 
         // add event listeners to change colors  
         div.addEventListener("mousedown", () => {
-            div.style.backgroundColor = color;
+            changeColor(div);
             mouseDown = true;
         })
         div.addEventListener("mouseup", () => {
             mouseDown = false;
         })
-        div.addEventListener("mousemove", () => {
+        div.addEventListener("mouseenter", () => {
             if(mouseDown) {
-                div.style.backgroundColor = color;
+                changeColor(div);
             }
         })
     }
@@ -63,38 +62,28 @@ function create () {
 
 //rainbow button
 let rainbow = document.getElementById("rainbow").
-    addEventListener("click", () => {
-    const randomR = Math.floor(Math.random() * 256)
-    const randomG = Math.floor(Math.random() * 256)
-    const randomB = Math.floor(Math.random() * 256)
-    color = `rgb(${randomR}, ${randomG}, ${randomB})`
-    })
-
+    addEventListener("click", () => { mode = 'rainbow';})   
+    
 //delete the color, eraser
 let eraser = document.getElementById("eraser").
-    addEventListener("click", () => {
-       color = "white"; 
-})
+    addEventListener("click", () => { mode = 'eraser'; })
 
-
-//dropdown button effects
-function drop() {
-    document.getElementById("dropdown").classList.toggle("show");
-}
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+function changeColor(div) {
+    if(mode === 'color') {
+        div.style.backgroundColor = color;
+    }
+    else if(mode === 'rainbow') {
+        let red = Math.random() * 255;
+        let green = Math.random() * 255;
+        let blue = Math.random() * 255;
+        div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    }
+    else if(mode === 'eraser') {
+        div.style.backgroundColor = 'white';
     }
 }
+
+
 
 let reload = document.querySelector("#reset").
     addEventListener("click", () => {
